6ef99ab6a4189a8e9b8f066bb8b5c38024bf9f55
Tyler Sebastian <tills13@gmail.com>
2023-11-15 16:49:49 -0800

    wip

--- cloudflare/constants.ts
+++ cloudflare/constants.ts
@@ -1,5 +1,5 @@
-export const ACCOUNT_ID = process.env.ACCOUNT_ID!;
-export const ZONE_ID = process.env.ZONE_ID!;
+export const ACCOUNT_ID = process.env.CF_ACCOUNT_ID!;
+export const ZONE_ID = process.env.CF_ZONE_ID!;
 export const CF_PAGES_PROJECT_ID = process.env.CF_PAGES_PROJECT_ID!;
 export const CF_AUTH_KEY = process.env.CF_AUTH_KEY!;
 export const CF_AUTH_EMAIL = process.env.CF_AUTH_EMAIL!;
