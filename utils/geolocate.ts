35c98c629e52c23a17d470acfa07de246258cbe6
Tyler Sebastian <tills13@gmail.com>
2025-03-14 12:32:21 -0700

    debug

--- utils/geolocate.ts
+++ utils/geolocate.ts
@@ -6,11 +6,21 @@ export async function geolocate(addr: string) {
   const u = new URL(GEOLOCATION_URL_BASE);
   u.searchParams.set(GEOLOCATION_QUERY_PARAM, addr);
 
-  const response = await fetch(u.href);
-  const rJson = await response.json();
+  try {
+    const response = await fetch(u.href);
 
-  return {
-    lat: parseFloat(rJson[0].lat),
-    long: parseFloat(rJson[0].lon),
-  };
+    console.log(response.status, response.statusText, await response.text());
+
+    const rJson = await response.json();
+
+    console.log({ rJson });
+
+    return {
+      lat: parseFloat(rJson[0].lat),
+      long: parseFloat(rJson[0].lon),
+    };
+  } catch (e) {
+    console.log(e);
+    return {};
+  }
 }
